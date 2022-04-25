aae99d559e87d2af04cc8f649f80ebc9856c3f16
Gabriel Oliveira <gabrieloliveirasantos196@gmail.com>
2022-04-24 22:03:38 -0300

    Criado Função para listar todos os filmes

--- cinema.js
+++ cinema.js
@@ -1,3 +1,12 @@
 var catalogo = require("./catalogo.json")
-console.log(catalogo)
 console.log(catalogo.data)
+var catalogoString = JSON.stringify(catalogo);
+var catalogoObj = JSON.parse(catalogoString);
+// console.log(catalogoObj)
+
+function ListarTodosOsFilmes(filmes) {
+    for (let i = 0; i < filmes.length; i++) {
+        console.log(filmes[ i ].titulo);
+    }
+}
+ListarTodosOsFilmes(catalogoObj.data);
